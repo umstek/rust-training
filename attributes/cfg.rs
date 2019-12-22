89702ae61d43c31e1fbe095e0e5683f70b96fc98
Wickramaranga Abeygunawardhana <umstek@live.com>
2019-12-22 13:51:15 +0530

    Add 'attributes'

---
+++ attributes/cfg.rs
@@ -0,0 +1,34 @@
+// This function only gets compiled if the target OS is linux
+#[cfg(target_os = "linux")]
+fn are_you_on_linux() {
+    println!("You are running linux!");
+}
+
+// And this function only gets compiled if the target OS is *not* linux
+#[cfg(not(target_os = "linux"))]
+fn are_you_on_linux() {
+    println!("You are *not* running linux!");
+}
+
+fn main() {
+    are_you_on_linux();
+    
+    println!("Are you sure?");
+    if cfg!(target_os = "linux") {
+        println!("Yes. It's definitely linux!");
+    } else {
+        println!("Yes. It's definitely *not* linux!");
+    }
+}
+
+// #[cfg(some_condition)]
+// fn conditional_function() {
+//     println!("condition met!");
+// }
+
+// fn main() {
+//     conditional_function();
+// }
+
+// $ rustc --cfg some_condition custom.rs && ./custom
+// condition met!
